3b7f85b8c0d89857be22e9f61c17da45d71f4487
hel99 <helena.aganovic99@gmail.com>
2023-10-29 00:04:32 +0200

    izmene

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const menu = document.querySelector(".menu");
 const hamburger = document.querySelector(".hamburger");
+const closeButton = document.querySelector(".closeButton");
+const footer = document.querySelector("footer");
+let trenutnaGodina = new Date().getFullYear();
 
-console.log(menu);
-console.log(hamburger);
+footer.textContent = `© ${trenutnaGodina} Požega`;
 
 function otvoriMenu() {
 	if (menu.classList.contains("showMenu")) {
 		menu.classList.remove("showMenu");
-		
+		hamburger.classList.remove("hideHamburger");
+		closeButton.classList.remove("showCloseButton");
 	} else {
 		menu.classList.add("showMenu");
-		console.log("work");
-	
+		hamburger.classList.add("hideHamburger");
+		closeButton.classList.add("showCloseButton");
 	}
 }
 
-
-
+hamburger.addEventListener("click", otvoriMenu);
 closeButton.addEventListener("click", otvoriMenu);
