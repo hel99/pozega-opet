91b30c05e8329892fbcac57428cabdca37edc249
radisa.bogdanovic <radisa.bogdanovic@algometric.net>
2023-11-08 15:34:00 +0100

    i anja isto da testira

--- app.js
+++ app.js
@@ -1,50 +1,79 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1").then(response =>{
-if(response.ok){
-	return response.json();
-} else{
-	throw new Error("interner konekcija nije u redu");
-}
+fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw new Error("interner konekcija nije u redu");
+	}
 });
-
+console.log(window.location);
+const divCustom = document.getElementById("customDiv");
+let h1 = document.createElement("h1");
+const submitButton = document.getElementById("submitbutton");
 const menu = document.querySelector(".menu");
 const hamburger = document.querySelector(".hamburger");
 const closeButton = document.querySelector(".closeButton");
 const footer = document.querySelector("footer");
 let trenutnaGodina = new Date().getFullYear();
-var popupWindow = document.getElementById("popupWindow");
-var closePopupButton = document.getElementById("closePopupButton");
-var newPopupWindow = document.getElementById("newPopupWindow");
-var closeNewPopupButton = document.getElementById("closeNewPopupButton");
-var formData={};
-var popupViseDetalja = document.getElementById("popupViseDetalja");
-var closeViseDetaljaPopupButton = document.getElementById("closeViseDetaljaPopupButton");
+const popupWindow = document.getElementById("popupWindow");
+const closePopupButton = document.getElementById("closePopupButton");
+const newPopupWindow = document.getElementById("newPopupWindow");
+const closeNewPopupButton = document.getElementById("closeNewPopupButton");
+const formData = {};
+const popupViseDetalja = document.getElementById("popupViseDetalja");
+let isDisabled = true;
+console.log(divCustom);
+divCustom.appendChild(h1);
+divCustom.removeChild(h1);
+h1.textContent = "hej ja sam ovde";
 
+let ime = "";
+let email = "";
+let naslov = "";
+let poruka = "";
+
+const closeViseDetaljaPopupButton = document.getElementById(
+	"closeViseDetaljaPopupButton"
+);
+const pathName = "/index.html";
+const contactPathName = '"/contact.html"';
 function showPopup() {
-    popupWindow.style.display = "block";
+	if (window.location.pathname === pathName) {
+		popupWindow.style.display = "block";
+	}
+}
+
+if (window.location.pathname === contactPathName) {
+	ime = document.getElementById("ime").value;
+	email = document.getElementById("email").value;
+	naslov = document.getElementById("naslov").value;
+	poruka = document.getElementById("poruka").value;
 }
 
 function closePopup() {
-    popupWindow.style.display = "none";
+	popupWindow.style.display = "none";
 }
 document.getElementById("buttonNo").addEventListener("click", function () {
-    window.open("https://www.lazalazarevic.rs/", "_blank");
+	window.open("https://www.lazalazarevic.rs/", "_blank");
 });
 document.getElementById("buttonYes").addEventListener("click", function () {
-		popupWindow.style.display = "none";
-		newPopupWindow.style.display = "block";
+	popupWindow.style.display = "none";
+	newPopupWindow.style.display = "block";
 });
 function closeNewPopup() {
-    newPopupWindow.style.display = "none";
+	newPopupWindow.style.display = "none";
 }
 
 setTimeout(showPopup, 3000);
-
-document.getElementById("viseDetalja").addEventListener("click", function () {
-	popupViseDetalja.style.display = "block";
-});
-
-function closePopupViseDetalja(){
-	popupViseDetalja.style.display = "none";
+console.log(document.getElementById("buttonYes"));
+if (document.getElementById("viseDetalja")) {
+	document
+		.getElementById("viseDetalja")
+		.addEventListener("click", function () {
+			popupViseDetalja.style.display = "block";
+		});
+	function closePopupViseDetalja() {
+		popupViseDetalja.style.display = "none";
+	}
 }
 
 footer.textContent = `© ${trenutnaGodina} Požega`;
@@ -60,26 +89,36 @@ function otvoriMenu() {
 		closeButton.classList.add("showCloseButton");
 	}
 }
-
-function saveData(){
-var ime = document.getElementById("ime").value;
-var email = document.getElementById("email").value;
-var naslov = document.getElementById("naslov").value;
-var poruka = document.getElementById("poruka").value;
-
-if(ime && email && naslov && poruka){
-formData.ime = ime;
-formData.email = email;
-formData.naslov = naslov;
-formData.poruka = poruka;
-alert("Pitanje je poslato.");
-} else {
-	alert("Molimo vas da popunite sva polja forme.");
+function onInputChange(e) {
+	const value = e.target.value;
+	submitButton.disabled = false;
+	if (value) {
+		ime = value;
+	}
 }
+function saveData() {
+	if (ime && email && naslov && poruka) {
+		formData.ime = ime;
+		formData.email = email;
+		formData.naslov = naslov;
+		formData.poruka = poruka;
+		submitButton.disabled = false;
+
+		alert("Pitanje je poslato.");
+		console.log(formData);
+	} else {
+		alert("Molimo vas da popunite sva polja forme.");
+		isDisabled = true;
+		return;
+	}
 }
+const name = "milojica".length;
+console.log(name);
 
 hamburger.addEventListener("click", otvoriMenu);
 closeButton.addEventListener("click", otvoriMenu);
 closePopupButton.addEventListener("click", closePopup);
 closeNewPopupButton.addEventListener("click", closeNewPopup);
-closeViseDetaljaPopupButton.addEventListener("click", closePopupViseDetalja);
+if (document.getElementById("viseDetalja")) {
+	closeViseDetaljaPopupButton.addEventListener("click", closePopupViseDetalja);
+}
